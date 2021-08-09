e523e0075f10edeeaffa41267e5ead0416a9f5dc
Toygar Aksoy <toygar.aksoy@gmail.com>
2021-08-09 21:22:37 +0300

    redundant codes cleaned

--- scrap.py
+++ scrap.py
@@ -9,13 +9,4 @@ soup = BeautifulSoup(page, 'html.parser')
 
 print("Title:", soup.title.string)
 
-# print(soup.get_text())
-# print(soup.find_all("div", attrs={"drop-cap-letter":True}))
-# for i in soup.find_all("p", attrs={"data-el": "text"}): print(i)
-
-# print(soup.find_all("p"))
-
-# for p in soup.select(".font--article-body"):
-#     print(p.text)
-
 print(" ".join([i.text for i in soup.select(".font--article-body")]))
